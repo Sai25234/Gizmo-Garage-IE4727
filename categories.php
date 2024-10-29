7cde33ddbdec3cfd1a47e5d3b6409baf4a057663
arielgohsn <145362950+arielgohsn@users.noreply.github.com>
2024-10-29 16:44:16 +0800

    create sample categories.php from html

---
+++ categories.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>View Products</title>
+  <link rel="stylesheet" href="css/main.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <div class="running-promo-banner">Running Promotion Banner</div>
+    <div class="top-bar">
+      <div class="logo">
+        <a href="index.html"><img src="images/gizmogaragelogo.png" alt="Gizmo Garage" /></a>
+      </div>
+      <div class="search-bar">
+        <select>
+          <option value="all">BY CATEGORY</option>
+          <option value="laptops">LAPTOPS</option>
+          <option value="desktops">DESKTOPS</option>
+          <option value="phones">PHONES</option>
+          <option value="tablets">TABLETS</option>
+          <option value="accessories">ACCESSORIES</option>
+        </select>
+        <input type="text" placeholder="Search for a Product..." />
+        <button>
+          <img src="images/search_icon.png" alt="Search" height="40px" width="40px" />
+        </button>
+      </div>
+      <div class="account-cart">
+        <a href="#" class="account-link">
+          <span class="material-symbols-outlined">
+            person
+          </span>
+          MY ACCOUNT
+        </a>
+        <a href="cart.html" class="cart-link">
+          <span class="material-symbols-outlined">
+            shopping_cart
+          </span>
+          CART
+        </a>
+      </div>
+    </div>
+    <nav class="nav-bar">
+      <a href="categories.html?category=laptops"
+        >LAPTOPS<span class="material-symbols-outlined">
+          keyboard_arrow_down
+        </span></a
+      >
+      <a href="categories.html?category=desktops"
+        >DESKTOPS<span class="material-symbols-outlined">
+          keyboard_arrow_down
+        </span></a
+      >
+      <a href="categories.html?category=phones"
+        >PHONES<span class="material-symbols-outlined">
+          keyboard_arrow_down
+        </span></a
+      >
+      <a href="categories.html?category=tablets"
+        >TABLETS<span class="material-symbols-outlined">
+          keyboard_arrow_down
+        </span></a
+      >
+      <a href="categories.html?category=accessories"
+        >ACCESSORIES<span class="material-symbols-outlined">
+          keyboard_arrow_down
+        </span></a
+      >
+      <a href="categories.html?sale=yes" class="sale-link"
+        >SALE<span class="material-symbols-outlined">
+          keyboard_arrow_down
+        </span></a
+      >
+    </nav>
+  </header>
+  <div class="categories-container">
+    <div class="filter-section">
+      <div id="filter-header">
+        <h3>SEARCH FILTER</h3>
+        <span class="material-symbols-outlined">tune</span>
+      </div>
+      <h4>PRODUCT CATEGORIES</h4>
+      <label><input type="checkbox" name="category" value="laptops"> Laptops</label>
+      <label><input type="checkbox" name="category" value="desktops"> Desktops</label>
+      <label><input type="checkbox" name="category" value="phones"> Phones</label>
+      <label><input type="checkbox" name="category" value="tablets"> Tablets</label>
+      <label><input type="checkbox" name="category" value="accessories"> Accessories</label>
+
+      <h4>BRANDS</h4>
+      <label><input type="checkbox" name="brand" value="brandA"> Brand A</label>
+      <label><input type="checkbox" name="brand" value="brandB"> Brand B</label>
+      <label><input type="checkbox" name="brand" value="brandC"> Brand C</label>
+
+      <h4>SORT BY</h4>
+      <label><input type="radio" name="sort" value="relevance"> Relevance</label>
+      <label><input type="radio" name="sort" value="latest"> Latest</label>
+      <label><input type="radio" name="sort" value="low-to-high"> Price: Low to High</label>
+      <label><input type="radio" name="sort" value="high-to-low"> Price: High to Low</label>
+    </div>
+
+    <div class="product-grid">
+      <?php 
+      @ $db = new mysqli('localhost', 'root', '', 'javajam');
+
+      if (mysqli_connect_errno()) {
+      echo 'Error: Could not connect to database.  Please try again later.';
+      exit;
+      }
+      if (isset($_GET['category'])) {
+        $category = $_GET['category'];
+        $categoryquery = "SELECT * FROM products WHERE category = '$category'";
+      } else {
+        //Failsafe to load all products if no category query found
+        $categoryquery = "SELECT * FROM products";
+      }
+      $result = $db->query($categoryquery);
+        
+      while ($row = $result->fetch_assoc()){
+        echo '<div class="product-item">';
+        echo '<img src="images/' . $row['image'] . '" alt="' . $row['product name'] . '">';
+        echo '<div class="product-item-body">';
+        echo '<div class="product-item-text">';
+        echo '<p class="product-name">' . $row['product name'] . '</p>';
+        echo '<p class="price">$' . $row['price'] . '</p></div>';
+        echo '<a href="#"><span class="material-symbols-outlined">shopping_cart</span></a>';
+        echo '</div></div>';
+      }
+      ?>
+    </div>
+  </div>
+
+  <div class="pagination">
+    <a href="#">««</a>
+    <a href="#" class="active">1</a>
+    <a href="#">2</a>
+    <a href="#">3</a>
+    <a href="#">»»</a>
+  </div>
+  </div>
+
+  <footer>
+    <div class="footer-container">
+      <div class="footer-column">
+        <h4><u>Contact Us</u></h4>
+        <p>+65 1234 5678</p>
+        <p>Mon - Fri 9am-6pm<br>(excl. public holidays)</p>
+        <p>gizmogarage@gmail.com</p>
+      </div>
+      <div class="footer-column">
+        <h4><u>Our Products</u></h4>
+        <ul>
+          <li><a href="#">Category A</a></li>
+          <li><a href="#">Category B</a></li>
+          <li><a href="#">Category C</a></li>
+          <li><a href="#">Category D</a></li>
+          <li><a href="#">Category E</a></li>
+          <li><a href="#" class="sale">Sale</a></li>
+        </ul>
+      </div>
+      <div class="footer-column">
+        <h4><u>Customer Service</u></h4>
+        <ul>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Terms of Service</a></li>
+          <li><a href="#">FAQs</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <div class="footer-column">
+        <h4><u>My Gizmo Garage</u> </h4>
+        <ul>
+          <li><a href="#">My Account</a></li>
+          <li><a href="#">Track Your Order</a></li>
+          <li><a href="#">Sustainability</a></li>
+          <li><a href="#">The Gizmo Newsletter</a></li>
+        </ul>
+      </div>
+      <div class="newsletter-column">
+        <h4><u>Join Our Newsletter</u> !</h4>
+        <form class="newsletter-form">
+          <input type="email" placeholder="Enter Email Address">
+          <button type="submit">&#10148;</button>
+        </form>
+        <p>Stay up-to-date with the latest news, products and exclusive deals.</p>
+        <div class="payment-methods">
+          <img src="images/visa_logo.png" alt="Visa">
+          <img src="images/mastercard_logo.png" alt="MasterCard">
+          <img src="images/paynow_logo.png" alt="PayNow">
+          <img src="images/Grab_pay_logo.png" alt="GrabPay">
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <p>© 2024 A Website by Ariel & Sai</p>
+    </div>
+  </footer>
+</body>
+
+</html>
