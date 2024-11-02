3515cfe840fc9c6478db4d8dde54a144a6af71f7
arielgohsn <145362950+arielgohsn@users.noreply.github.com>
2024-11-02 22:45:00 +0800

    update db_schema and insert

--- insert.sql
+++ insert.sql
@@ -1,16 +1,19 @@
-INSERT INTO products (ProductID, ProductName, Brand, Category, Image_url, Price, Stock) VALUES 
-(2, 'Apple 2024 MacBook Air', 'Apple', 'Laptops', 'https://m.media-amazon.com/images/I/71-D1xCuVwL.jpg', 1500, 100),
-(3, 'HP EliteBook 640', 'HP', 'Laptops', 'https://sg-media.apjonlinecdn.com/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/c/0/c08473069_4.png', 1559, 100),
-(4, 'HP Pavilion x360', 'HP', 'Laptops', 'https://sg-media.apjonlinecdn.com/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/c/0/c08908326_3_1.png', 1349, 200),
-(5, 'HP Spectre x360', 'HP', 'Laptops', 'https://sg-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/2/3/23c2_spectre_herbie_16_nightfall_black_t_9mp_ir_cam_nonfpr_avicii_2b_win11_core_set_tent_whitebg_touch_evoultra.png', 3499, 150),
-(6, 'HP Envy x360', 'HP', 'Laptops', 'https://sg-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08891498-touch.png', 1399, 100),
-(7, 'HP ProBook 440', 'HP', 'Laptops', 'https://sg-media.apjonlinecdn.com/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/a/7/a77nspt-u85zfe_bundle.png', 1839, 150),
-(8, 'Apple iPhone 16', 'Apple', 'Phones', 'https://down-sg.img.susercontent.com/file/sg-11134301-7rdx4-m014vsvi1vf9d3@resize_w900_nl.webp', 1449, 100),
-(9, 'ASUS ZenBook 14', 'ASUS', 'Laptops', 'https://m.media-amazon.com/images/I/61sOPUk8FNL._AC_SL1300_.jpg', 1117, 300),
-(10, 'ASUS VivoBook 14', 'ASUS', 'Laptops', 'https://m.media-amazon.com/images/I/71vKxjA8dsL.__AC_SX300_SY300_QL70_ML2_.jpg', 561, 180),
-(11, 'ASUS Chromebook Plus CX34', 'ASUS', 'Laptops', 'https://m.media-amazon.com/images/I/61MWHGFXhIL._AC_SX679_.jpg', 697, 150),
-(12, 'Lenovo V14 Gen 4', 'Lenovo', 'Laptops', 'https://m.media-amazon.com/images/I/81HBC8P2a1L._AC_SX679_.jpg', 545, 120),
-(13, 'Lenovo IdeaPad 3', 'Lenovo', 'Laptops', 'https://m.media-amazon.com/images/I/6183YBeJJFL._AC_SX679_.jpg', 583, 250),
-(14, 'Lenovo Thinkbook 16 Ultra 7', 'Lenovo', 'Laptops', 'https://m.media-amazon.com/images/I/81k6SvHC23L._AC_SX679_.jpg', 1589, 340),
-(15, 'Lenovo Legion Slim 5', 'Lenovo', 'Laptops', 'https://m.media-amazon.com/images/I/61cGV9ATZxL._AC_SX679_.jpg', 1646, 200),
-(16, 'Dell Vostro 3000 Series 3520', 'Dell', 'Laptops', 'https://m.media-amazon.com/images/I/71R2LA5tZJL._AC_SX679_.jpg', 1294, 180);
+INSERT INTO products (ProductName, Brand, Category, Image_url, Price, Stock) VALUES 
+('Apple 2024 MacBook Air', 'Apple', 'Laptops', 'https://m.media-amazon.com/images/I/71-D1xCuVwL.jpg', 1500, 100),
+('HP EliteBook 640', 'HP', 'Laptops', 'https://sg-media.apjonlinecdn.com/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/c/0/c08473069_4.png', 1559, 100),
+('HP Pavilion x360', 'HP', 'Laptops', 'https://sg-media.apjonlinecdn.com/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/c/0/c08908326_3_1.png', 1349, 200),
+('HP Spectre x360', 'HP', 'Laptops', 'https://sg-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/2/3/23c2_spectre_herbie_16_nightfall_black_t_9mp_ir_cam_nonfpr_avicii_2b_win11_core_set_tent_whitebg_touch_evoultra.png', 3499, 150),
+('HP Envy x360', 'HP', 'Laptops', 'https://sg-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/c/0/c08891498-touch.png', 1399, 100),
+('HP ProBook 440', 'HP', 'Laptops', 'https://sg-media.apjonlinecdn.com/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/a/7/a77nspt-u85zfe_bundle.png', 1839, 150),
+('Apple iPhone 16', 'Apple', 'Phones', 'https://down-sg.img.susercontent.com/file/sg-11134301-7rdx4-m014vsvi1vf9d3@resize_w900_nl.webp', 1449, 100),
+('ASUS ZenBook 14', 'ASUS', 'Laptops', 'https://m.media-amazon.com/images/I/61sOPUk8FNL._AC_SL1300_.jpg', 1117, 300),
+('ASUS VivoBook 14', 'ASUS', 'Laptops', 'https://m.media-amazon.com/images/I/71vKxjA8dsL.__AC_SX300_SY300_QL70_ML2_.jpg', 561, 180),
+('ASUS Chromebook Plus CX34', 'ASUS', 'Laptops', 'https://m.media-amazon.com/images/I/61MWHGFXhIL._AC_SX679_.jpg', 697, 150),
+('Lenovo V14 Gen 4', 'Lenovo', 'Laptops', 'https://m.media-amazon.com/images/I/81HBC8P2a1L._AC_SX679_.jpg', 545, 120),
+('Lenovo IdeaPad 3', 'Lenovo', 'Laptops', 'https://m.media-amazon.com/images/I/6183YBeJJFL._AC_SX679_.jpg', 583, 250),
+('Lenovo Thinkbook 16 Ultra 7', 'Lenovo', 'Laptops', 'https://m.media-amazon.com/images/I/81k6SvHC23L._AC_SX679_.jpg', 1589, 340),
+('Lenovo Legion Slim 5', 'Lenovo', 'Laptops', 'https://m.media-amazon.com/images/I/61cGV9ATZxL._AC_SX679_.jpg', 1646, 200),
+('Dell Vostro 3000 Series 3520', 'Dell', 'Laptops', 'https://m.media-amazon.com/images/I/71R2LA5tZJL._AC_SX679_.jpg', 1294, 180);
+
+INSERT INTO Admins (Email, Password) VALUES 
+('admin@garage.com', 'admin123');
