6ab7b41ec6b2b78ffcbbfebb463c3cec0e0a276f
arielgohsn <145362950+arielgohsn@users.noreply.github.com>
2024-11-05 17:16:57 +0800

    update session.php

--- session.php
+++ session.php
@@ -1,4 +1,6 @@
 <?php
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 $_SESSION['cart'] = $_SESSION['cart'] ?? array('items' => array(), 'qty' => array());
 ?>
