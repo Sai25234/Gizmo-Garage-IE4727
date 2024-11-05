828b0af20d9babc6e9a292d531fffc369db92783
arielgohsn <145362950+arielgohsn@users.noreply.github.com>
2024-11-05 16:31:40 +0800

    create database_initialisation.sql

--- database_initialisation.sql
+++ database_initialisation.sql
@@ -1,4 +1,67 @@
-INSERT INTO products (ProductName, Brand, Category, Image_url, Price, Stock, Specs) VALUES
+DROP TABLE IF EXISTS OrderItems;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Admins;
+DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS Promotions;
+DROP TABLE IF EXISTS Products;
+
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY AUTO_INCREMENT,
+    ProductName VARCHAR(100) NOT NULL,
+    Brand VARCHAR(100),
+    Category VARCHAR(100),
+    Image_url VARCHAR(1000),
+    Price DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
+    SalePrice DECIMAL(10,2) DEFAULT NULL,
+    Specs VARCHAR(500) DEFAULT NULL,
+    Stock INT NOT NULL
+);
+
+CREATE TABLE Promotions (
+    PromotionID INT PRIMARY KEY AUTO_INCREMENT,
+    Category VARCHAR(100),
+    Discount INT NOT NULL CHECK (Discount >= 0 AND Discount <= 100)
+);
+
+CREATE TABLE Customers (
+    Email VARCHAR(100) UNIQUE NOT NULL PRIMARY KEY,
+    Password VARCHAR(100) UNIQUE NOT NULL
+);  
+
+CREATE TABLE Admins(
+    AdminID INT PRIMARY KEY AUTO_INCREMENT,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    Password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY AUTO_INCREMENT,
+    CustomerName VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Phone VARCHAR(12) NOT NULL,
+    Address TEXT,
+    PaymentDetails TEXT,
+    Status VARCHAR(50) NOT NULL CHECK (Status IN ('Pending', 'Processing', 'Completed', 'Cancelled')),
+    Total DECIMAL(10, 2),
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (Email) REFERENCES Customers(Email) ON DELETE CASCADE
+);
+
+CREATE TABLE OrderItems (
+    OrderID INT,
+    ProductID INT,
+    Quantity INT NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (OrderID, ProductID),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID) ON DELETE CASCADE,
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+INSERT INTO Admins (Email, Password) VALUES 
+('admin@garage.com', MD5('admin123'));
+
+INSERT INTO Products (ProductName, Brand, Category, Image_url, Price, Stock, Specs) VALUES
 ('ASUS Chromebook Laptop Student Business 2024', 'ASUS', 'Laptops', 'https://m.media-amazon.com/images/I/61wR4i8il-L._AC_SX679_.jpg,https://m.media-amazon.com/images/I/81Bf5NNca6L._AC_SX679_.jpg,https://m.media-amazon.com/images/I/81bOqPWqWoL._AC_SX679_.jpg,https://m.media-amazon.com/images/I/81-obFV8NBL._AC_SX679_.jpg', 239.00, 80, 'Brand: ASUS,Model: 2024 Flagship ASUS Chromebook,Screen size: 14 Inches,Colour: Gravity Grey,Hard disk size: 64 GB,CPU model: Others,Installed RAM memory size: 4 GB,Operating system: Chrome OS'),
 ('Apple 2024 MacBook Air', 'Apple', 'Laptops', 'https://m.media-amazon.com/images/I/71-D1xCuVwL.jpg,https://down-sg.img.susercontent.com/file/sg-11134207-7rcbs-lss77touv6yb7d.webp,https://down-sg.img.susercontent.com/file/sg-11134207-7rcdd-lss77tov0t8360@resize_w900_nl.webp,https://down-sg.img.susercontent.com/file/sg-11134207-7rce5-lss77touy03776@resize_w900_nl.webp', 1500.00, 100, 'Screen Size: 13.6\" (2560 X 1664) Retina, Battery Life: Go All Day With Up To 18 Hours Of Battery Life,Dimensions (w X H X d): 1.13(h) X 30.41(w) X 21.5(d) cm,Speaker(s): FouR-Speaker Sound System'),
 ('HP EliteBook 640', 'HP', 'Laptops', 'https://sg-media.apjonlinecdn.com/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/c/0/c08473069_4.png,https://sg-media.apjonlinecdn.com/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/e/l/elitebook-640-g11-rex14-dimension_2.jpg,https://sg-media.apjonlinecdn.com/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/e/l/elitebook-640-g11-rex14-ports_2.jpg', 1859.00, 100, 'Brand: HP,Processor: Intel® Core™ Ultra 5 125U,Graphics: Intel® Graphics,Screen Size: 14\" diagonal WUXGA display,Memory and storage: 16 GB memory; 512 GB SSD storage,Operating system: Windows 11 Pro'),
@@ -15,7 +78,3 @@ INSERT INTO products (ProductName, Brand, Category, Image_url, Price, Stock, Spe
 ('Lenovo Thinkbook 16 Ultra 7', 'Lenovo', 'Laptops', 'https://m.media-amazon.com/images/I/81k6SvHC23L._AC_SX679_.jpg,https://m.media-amazon.com/images/I/51lha6kezzL._AC_SX679_.jpg,https://m.media-amazon.com/images/I/61LDY-coQsL._AC_SX679_.jpg,https://m.media-amazon.com/images/I/51oteyCWzmL._AC_SX679_.jpg', 1589.00, 340, 'Brand: Lenovo,Model: ThinkBook,Screen size: 16Inches,Colour:	ArcticGrey,Hard disk size:	1TB,CPU model:	Intel Corei5,Installed RAM memory size:	16GB,Operating system: Windows 11 Pro'),
 ('Lenovo Legion Slim 5', 'Lenovo', 'Laptops', 'https://m.media-amazon.com/images/I/61cGV9ATZxL._AC_SX679_.jpg,https://m.media-amazon.com/images/I/61j9iSNXCSL._AC_SX679_.jpg,https://m.media-amazon.com/images/I/61IWXjT9sKL._AC_SL1500_.jpg,https://m.media-amazon.com/images/I/51X+4Xmm4ZL._AC_SL1097_.jpg', 1646.00, 200, 'Brand:	Lenovo,Model:	Slim 5,Screen size:	16 Inches,Hard disk size:	512 GB,CPU model:	AMD Ryzen 7,Installed RAM memory size:	16 GB,Operating system: Windows 11 Home'),
 ('Dell Vostro 3000 Series ', 'Dell', 'Laptops', 'https://m.media-amazon.com/images/I/71R2LA5tZJL._AC_SX679_.jpg,https://m.media-amazon.com/images/I/71+wCbHlOqL._AC_SX679_.jpg,https://m.media-amazon.com/images/I/810dQSR2V3L._AC_SX679_.jpg,https://m.media-amazon.com/images/I/61b-R-Rtk7L._AC_SX679_.jpg', 1294.00, 180, 'Brand: Dell,Model name:	Vostro,Screen size:	15.6 Inches,Hard disk size:	2 TB,CPU model:	Intel Core i7,Installed RAM memory size:	64 GB,Operating system: Windows 11 Pro');
-
-
-INSERT INTO Admins (Email, Password) VALUES 
-('admin@garage.com', MD5('admin123'));
